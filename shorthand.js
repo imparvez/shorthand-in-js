3b0c3b4d706ec73be6249bd4867101d2f2ba6e37
Parvez Shaikh <parvez.shaikh@bookmyshow.com>
2016-08-30 16:33:12 +0530

    if else shorthand

--- shorthand.js
+++ shorthand.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	alert("aSome");
+	var big = prompt("Enter your number from 1 to 20");
+	//Traditional Method
+	// if(big > 10) {
+	// 	alert("You input is greater than 10");
+	// } else {
+	// 	alert("You input is less than 10")
+	// }
+
+	//Shorthand
+	big >= 10 ? alert("You input is greater than 10") : alert("You input is less than 10");
+	/*Comparing the input(big) with the pre defined value*/
+
+
+	var small = window.prompt('Enter your next number');
+	small <= 20 ? alert("Number less than 20 or equal to 20") : small >= 30 ? alert("number is less than 30"): alert("number is greater than 30");
+	/*Comparing more condition using shorthand for if else chain*/
 });
